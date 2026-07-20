6a938c02d4919da6f48b85e98d813d280895152f
Quang-LV1507 <levanquangkg2001@gmail.com>
2026-07-20 14:43:14 +0700

    Update gallery.js

--- accessible-photo-gallery/gallery.js
+++ accessible-photo-gallery/gallery.js
@@ -1,1 +1,36 @@
+function upDate(previewPic) {
+    console.log("The upDate event was triggered.");
+    console.log("Alternative text:", previewPic.alt);
+    console.log("Image source:", previewPic.src);
 
+    const imageDiv = document.getElementById("image");
+
+    imageDiv.textContent = previewPic.alt;
+    imageDiv.style.backgroundImage =
+        `url("${previewPic.src}")`;
+}
+
+function unDo() {
+    console.log("The unDo event was triggered.");
+
+    const imageDiv = document.getElementById("image");
+
+    imageDiv.style.backgroundImage = "url('')";
+    imageDiv.textContent =
+        "Hover over or focus on an image below to display here.";
+}
+
+function addTabFocus() {
+    console.log("The page load event was triggered.");
+
+    const images = document.querySelectorAll(".preview");
+
+    for (let index = 0; index < images.length; index++) {
+        images[index].setAttribute("tabindex", "0");
+
+        console.log(
+            "Added tabindex to image:",
+            index + 1
+        );
+    }
+}

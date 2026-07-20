8e657efba9d12543144bcff244adbccd153a68c9
Quang-LV1507 <levanquangkg2001@gmail.com>
2026-07-20 14:32:16 +0700

    Update gallery.js

--- JS-photo-gallery/gallery.js
+++ JS-photo-gallery/gallery.js
@@ -1,1 +1,20 @@
+function upDate(previewPic) {
+    console.log("Mouse entered an image.");
+    console.log("Alt text:", previewPic.alt);
+    console.log("Image source:", previewPic.src);
 
+    const imageDiv = document.getElementById("image");
+
+    imageDiv.textContent = previewPic.alt;
+    imageDiv.style.backgroundImage = `url("${previewPic.src}")`;
+}
+
+function unDo() {
+    console.log("Mouse left the image.");
+
+    const imageDiv = document.getElementById("image");
+
+    imageDiv.style.backgroundImage = "url('')";
+    imageDiv.textContent =
+        "Hover over an image below to display here.";
+}
